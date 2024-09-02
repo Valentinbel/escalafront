0cc769be8ef45288c16dd90d92dde20955048add
Valentin Belhomme <50097739+Valentinbel@users.noreply.github.com>
2024-09-02 22:27:49 +0200

    routing 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,40 @@ import { Routes } from '@angular/router';
 import { ClimberprofileComponent } from './climberprofile/climberprofile.component';
 import { SearchComponent } from './search/search.component';
 import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CreateClimberprofileComponent } from './climberprofile/create-climberprofile/create-climberprofile.component';
+
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'climber-profile', pathMatch:'full'},
-    {path: 'climber-profile', component: ClimberprofileComponent},
-    {path: 'searches', component: SearchComponent}, //have to be search by profile
-    {path: 'contact', component: ContactComponent},
+    {
+        path: '', 
+        redirectTo: 'climber-profile', 
+        pathMatch:'full'
+    },
+    {
+        path: 'climber-profile',
+        title: 'Profile', 
+        component: ClimberprofileComponent
+    },
+    {
+        path: 'add-climber-profile',
+        title: 'Add profile', 
+        component: CreateClimberprofileComponent
+    },
+    {
+        path: 'searches', 
+        title: 'Searches', 
+        component: SearchComponent 
+    }, 
+    // create search by profile
+    {
+        path: 'contact', 
+        title: 'Contact',
+        component: ContactComponent
+    },
+    { 
+        path: '**', 
+        title: '404', 
+        component: PageNotFoundComponent 
+    }
 ];
