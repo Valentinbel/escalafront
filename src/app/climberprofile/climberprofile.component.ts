327882c53e97c94621638081eb8fbc0bae856bb3
Valentin Belhomme <50097739+Valentinbel@users.noreply.github.com>
2024-09-05 14:41:33 +0200

    create climberProfile v1

--- src/app/climberprofile/climberprofile.component.ts
+++ src/app/climberprofile/climberprofile.component.ts
@@ -1,22 +1,50 @@
 import { Component } from '@angular/core';
 import { ClimberprofileService } from './../climberprofile/climberprofile.service';
-import { RouterModule } from '@angular/router';
+import { RouterModule, ActivatedRoute, Router } from '@angular/router';
+import { ClimberProfile } from '../model/climberprofile.model';
+import { Observable} from 'rxjs';
+import { state } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-climberprofile',
   standalone: true,
-  imports: [ RouterModule],
+  imports: [ RouterModule, CommonModule],
   templateUrl: './climberprofile.component.html',
   styleUrl: './climberprofile.component.css'
 })
 export class ClimberprofileComponent {
-  constructor(private climberprofileService: ClimberprofileService){}
+
+  climberProfile: ClimberProfile;
+  
+  constructor(
+    private climberprofileService: ClimberprofileService, 
+    private router: Router,
+    private route: ActivatedRoute){}
 
   ngOnInit(){
-    console.log("Salut les amis");
-    this.climberprofileService.getAllClimberProfiles().subscribe((climberProfile) => console.log("getAllClimberProfiles: " , climberProfile));
+    console.log("Salut ClimberprofileComponent");
+    //  récupérer le ClimberProfile grâce au User en amont. 
+  
+    this.getClimberProfileById(1);
+  }
 
-    this.climberprofileService.getClimberProfileById(1).subscribe((climberProfile) => console.log("getClimberProfileById(1): ", climberProfile));
-      }
+  getClimberProfileById(id: number): void {
+    this.climberprofileService.getClimberProfileById(id).subscribe((climberProfile) =>{
+      this.climberProfile =  climberProfile;
+      console.log(this.climberProfile);
+    } );
+  }
+
+  openRubrique() {
+    // this.matomoService.updateUrl('/rubrique');
+    //this.router.navigate(['/add-climber-profile'], { state: { contenu } });
+
+    this.router.navigate(
+      ['/add-climber-profile']
+      //{queryParams:{order:'popular','price-range':'expensive'}}
+      //state: { hello: 'world'}
+      );
+}
 }
 
