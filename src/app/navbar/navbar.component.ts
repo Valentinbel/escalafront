fd1a7c162771fa00fb8e7b24c94e7a742974df15
vbelhomm <valentinbelhomme@gmail.com>
2026-01-20 17:23:37 +0100

    feat(avatarNavbar): avatarNavbar v1

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit, inject } from '@angular/core';
+import {Component, OnInit, inject, OnDestroy} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 import { AuthStorageService } from '../auth/auth-storage.service';
-import { Subscription } from 'rxjs';
+import {Subject, Subscription, takeUntil} from 'rxjs';
 import { EventBusService } from '../shared/event-bus.service';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { SnackBarService } from '../shared/snack-bar/snack-bar.service';
 import { MessageResponse } from '../model/message-response.model';
 import { LanguagesComponent } from './languages/languages.component';
+import {AvatarStorageService} from "../shared/avatar/avatar-storage.service";
+import {DomSanitizer, SafeStyle, SafeUrl} from "@angular/platform-browser";
+import {AvatarService} from "../shared/avatar/avatar.service";
+import {NgClass} from "@angular/common";
 
 @Component({
     selector: 'navbar',
-    imports: [RouterModule, TranslateModule, LanguagesComponent],
+  imports: [RouterModule, TranslateModule, LanguagesComponent, NgClass],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit {
-
-  eventBusSub?: Subscription;
-  isLoggedIn = false;
+export class NavbarComponent implements OnInit, OnDestroy {
 
   // Service injecté != constructor
   authStorageService = inject(AuthStorageService);
@@ -26,9 +27,20 @@ export class NavbarComponent implements OnInit {
   eventBusService = inject(EventBusService);
   translateService = inject(TranslateService)
   snackBarService = inject(SnackBarService);
+  avatarService = inject(AvatarService);
+  avatarStorageService = inject(AvatarStorageService);
+  sanitizer = inject(DomSanitizer);
+
+  eventBusSub?: Subscription;
+  isLoggedIn = false;
+  avatarBackgroundStyle: SafeStyle | null = null;
+  objectUrl: string | null = null;
+  avatarUrl: SafeUrl | null = null;
 
   // Accès au signal username (lecture seule)
   readonly userNameSignal = this.authStorageService.username;
+  private readonly destroy$ = new Subject<void>();
+  private userId: number;
 
   constructor(){}
 
@@ -39,6 +51,31 @@ export class NavbarComponent implements OnInit {
 
     if (this.authStorageService.isLoggedIn()) {
       this.isLoggedIn = true;
+      this.userId = this.authStorageService.getUserId();
+
+      if (this.avatarStorageService.getAvatarId())
+        //this.avatarBackgroundStyle =
+        this.avatarService.getFile(this.userId)
+          .pipe(takeUntil(this.destroy$)).subscribe({
+            next: (url) => {
+              // Nettoyer l'ancienne URL si elle existe
+              if (this.objectUrl) {
+                URL.revokeObjectURL(this.objectUrl);
+              }
+
+              // Stocker l'URL brute
+              this.avatarUrl = url;
+
+              // Sanitizer pour background-image
+              const backgroundImageValue = `url(${url})`;
+              this.avatarBackgroundStyle = this.sanitizer.bypassSecurityTrustStyle(backgroundImageValue);
+              //return this.avatarBackgroundStyle;
+            },
+            error: (err) => {
+              console.log("Error on retrieving avatar from server");
+              console.log(err);
+            }
+          });
     }
   }
 
@@ -60,5 +97,16 @@ export class NavbarComponent implements OnInit {
       });
     }
     this.isLoggedIn = false;
+  }
+
+  ngOnDestroy(): void {
+    // clean subscription
+    this.destroy$.next();
+    this.destroy$.complete();
+
+    // Libération de la mémoire de l'URL objet
+    if (this.avatarUrl) {
+      URL.revokeObjectURL(this.avatarUrl.toString());
+    }
   }
 }
